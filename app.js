43d7359aa754324ea539106f8f076d131c4aaf7c
ishrat <siddiqueishrat36@gmail.com>
2025-06-20 17:27:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+// question no 01: declear a variable called age and assign to it your age.show your age in an alert box?
+var age = 41;
+alert("my age is " + age);
+
+// // question no 02: declear and initialize a variable to keep track of how many times a vistor has visited your page.
+
+var visitCount = 14;
+alert("you have visited this site " + visitCount + "times");
+
+// // question no 03: declear a variable called birthYear and assign to it your birth year. show the following message in your brower's?
+var birthYear =1982;
+document.write("My birth year is " + birthYear + "<br> Data type of my decleared variable is number");
+
+// question no 04: A vistor visits an online clothing StorageEvent.
+
+var visitorName = "Ishrt siddiqui";
+var productTitle = "T-shirt";
+var quantity = 5;
+document.write(" vistor's name is " + visitorName + "\nproduct tittle is " +
+    productTitle + " quantity is " + quantity + visitorName   + quantity + " " + productTitle + 
+"(s) on XYZ clothing store");
+// question no 01: Declear 3 variables in one statment?
+var firstName = "Ishrat Siddiqui", lastName ="Siddiqui", age = 41;
+// question no 03: display this in your brower?
+document.write("<h4> Rules for naming JS variables</h4>");
+// 2: variable names can only contain  for example: $my_1stVariable is valid
+document.write("<p>Variable names can only contain letters, numbers, underscores, and dollar signs. for example : $my_1stVariable is valid.</p>")
+// 3: variable must begin with a for example: $name, _name or name are all valid
+document.write("<p> Variable must begin with a for example: $name, _name or name are all valid.</p>");
+// 4: variable names are case?
+document.write("<p> variable names are case-sensitive for example: myVariable and myvariable are different variables.</p>");
+// 5: variable names should not be JS ?
+document.write("<p> variable names should not be JS for example: var, let, const are reserved keyword and cannot be used as variable names.</p>");
+
+
+
+
+
+
+
+
